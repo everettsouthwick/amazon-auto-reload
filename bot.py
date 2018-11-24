d3866c9c5310b1ab705a6836981091ce71673595
Everett Southwick <everett@everettsouthwick.com>
2018-11-24 13:00:04 -0600

    Update to conform to Amazon's new process

--- bot.py
+++ bot.py
@@ -26,50 +26,22 @@ def login():
     DRIVER.find_element_by_id('signInSubmit').click()
     reload()
 
-def resolve_payment_method():
-    print('Resolving payment method...')
-    try:
-        DRIVER.find_element_by_xpath('//*[@id="asv-payment-error-inline"]/div/div/div')
-        time.sleep(2)
-        DRIVER.find_element_by_id('asv-payment-edit-link').click()
-        WAIT.until(EC.title_is('Change payment method'))
-        time.sleep(2)
-        try:
-            DRIVER.find_elements_by_class_name('pmts-instrument-box')[2].click()
-            time.sleep(2)
-            DRIVER.find_element_by_name('addCardNumber').send_keys(CARD_NUMBER)
-            time.sleep(2)
-            DRIVER.find_element_by_xpath("//button[contains(.,'Confirm Card')]").click()
-            time.sleep(2)
-            DRIVER.find_element_by_xpath("//*[@id='asv-form-submit']").click()
-            time.sleep(2)
-            reload()
-        except NoSuchElementException:
-            print('ERR: Failed to verify payment method.')
-    except NoSuchElementException:
-        print('ERR: No in-line error found.')
-
 def reload():
     print('Reloading balance...')
-    DRIVER.get('https://smile.amazon.com/asv/reload/')
-    WAIT.until(EC.title_is('Reload Your Balance'))
-    time.sleep(2)
+    try:
+        DRIVER.find_elements_by_class_name('pmts-credit-card-row')[2].click()
+        time.sleep(2)
+        DRIVER.find_element_by_xpath("//input[@placeholder='ending in 6625']").send_keys(CARD_NUMBER)
+        time.sleep(2)
+        DRIVER.find_elements_by_xpath("//button[contains(.,'Confirm Card')]")[2].click()
+    except:
+        quit()
     DRIVER.find_element_by_id('asv-manual-reload-amount').clear()
     time.sleep(2)
     DRIVER.find_element_by_id('asv-manual-reload-amount').send_keys('0.50')
     DRIVER.execute_script('document.querySelector("#asv-manual-reload-amount").blur();')
     time.sleep(2)
     DRIVER.find_element_by_xpath("//button[contains(.,'Reload $0.50')]").click()
-    time.sleep(2)
-
-    # If the payment did not go through, attempt to resolve the payment method.
-    if DRIVER.title != 'Thank you for reloading your balance':
-        try:
-            resolve_payment_method()
-        except NoSuchElementException:
-            print('Something went wrong...')
-            quit()
-
     WAIT.until(EC.title_is('Thank you for reloading your balance'))
     DRIVER.close()
     quit()
