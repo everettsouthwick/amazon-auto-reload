5fd8d79889c642edf7eb5019929b838006ad7d1d
Everett Southwick <everett@everettsouthwick.com>
2018-12-29 11:39:12 -0600

    Add a second card verification check to account for Amazon's new process.

--- amazon/core.py
+++ amazon/core.py
@@ -1,10 +1,12 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-def login(username, password, driver, wait):
+def login(username, password, driver):
+    wait = WebDriverWait(driver, 15)
     driver.get("https://smile.amazon.com/asv/reload/")
     driver.find_element_by_id("form-submit-button").click()
     wait.until(EC.title_is("Amazon Sign In"))
@@ -17,29 +19,53 @@ def login(username, password, driver, wait):
     driver.find_element_by_id("signInSubmit").click()
     wait.until(EC.title_is("Reload Your Balance"))
 
-def reload_card(card_number, last_four, reload_amount, driver, wait):
+def reload_card(card_number, last_four, reload_amount, driver):
+    wait = WebDriverWait(driver, 15)
     driver.get("https://smile.amazon.com/asv/reload/")
     
     # Select the correct credit card and verify it.
     driver.find_element_by_xpath("//*[contains(text(), 'ending in {}')]".format(last_four)).click()
-    time.sleep(1)
     try:
-        driver.find_element_by_xpath("//input[@placeholder='ending in {}']".format(last_four)).send_keys(card_number)
-        time.sleep(1)
-        confirmation_buttons = driver.find_elements_by_xpath("//button[contains(.,'Confirm Card')]")
-        for button in confirmation_buttons:
+        conf_input = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='ending in {}']".format(last_four)))
+        )
+        conf_input.send_keys(card_number)
+        conf_buttons = driver.find_elements_by_xpath("//button[contains(.,'Confirm Card')]")
+        for button in conf_buttons:
             try:
                 button.click()
                 break
             except:
                 pass
+    except TimeoutException:
+        pass
     except NoSuchElementException:
         pass
 
     # Reload for the specified amount.
     driver.find_element_by_id('asv-manual-reload-amount').clear()
     driver.find_element_by_id('asv-manual-reload-amount').send_keys('%.2f' % reload_amount)
-    time.sleep(1)
     driver.execute_script('document.querySelector("#asv-manual-reload-amount").blur();') # Blur the reload amount input to ensure the correct value will be reloaded.
     driver.find_element_by_xpath("//button[contains(.,'Reload ${}')]".format('%.2f' % reload_amount)).click()
+    
+    # Try again to verify the credit card.
+    try:
+        conf_input = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='ending in {}']".format(last_four)))
+        )
+        conf_input.send_keys(card_number)
+        conf_buttons = driver.find_elements_by_xpath("//button[contains(.,'Confirm Card')]")
+        for button in conf_buttons:
+            try:
+                button.click()
+                break
+            except:
+                pass
+        time.sleep(1)
+        driver.find_element_by_xpath("//button[contains(.,'Reload ${}')]".format('%.2f' % reload_amount)).click()
+    except TimeoutException:
+        pass
+    except NoSuchElementException:
+        pass
+
     wait.until(EC.title_is("Thank you for reloading your balance"))
--- run.py
+++ run.py
@@ -18,14 +18,13 @@ with open("{}\\config.json".format(os.path.dirname(os.path.realpath(sys.argv[0])
             cards.append(newCard)
 
 driver = webdriver.Chrome("{}\\chromedriver.exe".format(os.path.dirname(os.path.realpath(sys.argv[0]))))
-wait = WebDriverWait(driver, 30)
-amazon.login(username, password, driver, wait)
+amazon.login(username, password, driver)
     
 for card in cards:
     last_four = card.card_number[-4:]
     while card.reload_times > 0:
         print("Reloading card ending in {} with ${}.".format(last_four, '%.2f' % card.reload_amount))
-        amazon.reload_card(card.card_number, last_four, card.reload_amount, driver, wait)
+        amazon.reload_card(card.card_number, last_four, card.reload_amount, driver)
         card.reload_times -= 1
         if card.reload_times > 0:
             print("Pausing for {} seconds.".format(delay))
