99b86805cfea434b2c39c1adf9562529f2c1e6ac
ab763 <git-andrew.bond@xoxy.net>
2019-04-28 19:03:14 +0700

    Refactor for add'l vendors/card data providers
    No longer require manual installation of a webdriver instance - now
    handle everything with npm.
    
    Fix tiny bug in browser wait call if card number confirmation required
    
    Refactor to potentially allow for additional vendors where purchases can
    be made at, as well as to allow card data to be verified before using
    the card.
    
    Created example config.json file

--- app/models/card.js
+++ app/models/card.js
@@ -1,6 +1,25 @@
-module.exports = class Card {
-    constructor(options = {}) {
-        options.lastFour = options.cardNumber.slice(-4)
-        Object.assign(this, options)
-    }
+'use strict';
+
+class Card {
+	constructor(card) {
+		this.description = card.description;
+		this.cardNumber = card.cardNumber;
+		this.reloadAmount = card.reloadAmount;
+		this.cardholderName = card.cardholderName;
+		this.expirationMonth = card.expirationMonth;
+		this.expirationYear = card.expirationYear;
+		this.reloadTimes = card.reloadTimes;
+		this.enabled = card.enabled;
+		this.skip = !this.enabled;
+
+		if (isNaN(this.cardNumber) || isNaN(this.reloadAmount)) {
+			throw new TypeError('Invalid input card data');
+		}
+	}
+
+	get lastFour() {
+		return this.cardNumber.slice(-4);
+	}
 }
+
+module.exports = Card;
